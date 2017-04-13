62639f2cffc1a8f667d6af3099f818a12d7033e0
Mikhail Polyanskiy <mpolyanskiy@gmail.com>
2017-04-12 20:51:25 -0400

    Adachi - CdTe, Ozaki - ZnS

--- scripts/Adachi 1993 - CdTe.py
+++ scripts/Adachi 1993 - CdTe.py
@@ -1,88 +1,80 @@
 # -*- coding: utf-8 -*-
 # Author: Mikhail Polyanskiy
 # Last modified: 2017-04-12
-# Original data: Ozaki and Adachi 1995, https://doi.org/10.1063/1.359966
+# Original data: Adachi et al. 1993, https://doi.org/10.1063/1.354543
 
 import numpy as np
 import matplotlib.pyplot as plt
 π = np.pi
 
-# model parameters from table I
-E0   = 1.42    #eV
-Δ0   = 1.75-E0 #eV
-A    = 7.0     #eV**1.5
-Γ0   = 0.03    #eV
+# model parameters from table III
+E0  = 1.58    #eV
+Δ0  = 2.55-E0 #eV
+A   = 10      #eV**1.5
+A0x = 0.0034  #eV**-1
+G0  = 0.0065  #eV
+Γ0  = 0.01    #eV
 
-E1   = 2.91    #eV
-Δ1   = 3.14-E1 #eV
-B1   = 3.50
-B2   = 1.75
-B1x  = 1.2     #eV
-B2x  = 0.6     #eV
-Γ1   = 0.12    #eV
+E1  = 3.55    #eV
+Δ1  = 4.13-E1 #eV
+B1  = 2.0
+B1s = 0.6
+B1x = 1.56    #eV
+B1sx= 0.65    #eV
+G1  = 0.24    #eV
+GΔ  = 0.24    #eV
+Γ1  = 0.21    #eV
 
-E0pr = 4.45    #eV
-C0pr = 0.80
-Γ0pr = 0.67
+E2  = 5.13    #eV
+C   = 1.00
+γ   = 0.16
 
-E21  = 4.77    #eV
-C21  = 1.35
-Γ21  = 0.62
-
-E22  = 5.00    #eV
-C22  = 0.35
-Γ22  = 0.4     #eV
-
-E1pr = 6.6     #eV
-C1pr = 0.7
-Γ1pr = 0.6     #eV
-
+εinf= 1.27
 
 def Epsilon_A(ħω):
-    χ0 = (ħω + 1j*Γ0) / E0
-    χs0 = (ħω + 1j*Γ0) / (E0+Δ0)
-    fχ0 = χ0**-2 * ( 2  -(1+χ0)**0.5 - (1-χ0)**0.5 )
-    fχs0 = χs0**-2 * ( 2 - (1+χs0)**0.5 - (1-χs0)**0.5 )
+    χ0 = (ħω+1j*Γ0) / E0
+    χs0 = (ħω+1j*Γ0) / (E0+Δ0)
+    fχ0 = χ0**-2 * ( 2-(1+χ0)**0.5-(1-χ0)**0.5 )
+    fχs0 = χs0**-2 * ( 2-(1+χs0)**0.5-(1-χs0)**0.5 )
     return A*E0**-1.5 * (fχ0+0.5*(E0/(E0+Δ0))**1.5*fχs0)
 
-def Epsilon_B(ħω):
-    χ1d = (ħω + 1j*Γ1) / E1
-    χ1sd = (ħω + 1j*Γ1) / (E1+Δ1)
-    return -B1*χ1d**-2*np.log(1-χ1d**2) - B2*χ1sd**-2*np.log(1-χ1sd**2)
-
-def Epsilon_Bx(ħω):
+def Epsilon_Ax(ħω):
     y=0
     for n in range(1,1000):
-       y += 1/(2*n-1)**3 * ( B1x/(E1-ħω-1j*Γ1) + B2x/(E1+Δ1-ħω-1j*Γ1) )
-       #note: G1 = 0; G1s = 0
+        y += A0x/n**3 * ( 1/(E0-G0/n**2-ħω-1j*Γ0) + 0.5/(E0+Δ0-G0/n**2-ħω-1j*Γ0) )
     return y
 
-def Epsilon_C0pr(ħω):
-    return C0pr*E0pr**2/(E0pr**2-ħω**2-1j*ħω*Γ0pr)
+def Epsilon_B(ħω):
+    χ1 = (ħω+1j*Γ1) / E1
+    χ1s = (ħω+1j*Γ1) / (E1+Δ1)
+    return -B1*χ1**-2*np.log(1-χ1**2) - B1s*χ1s**-2*np.log(1-χ1s**2) #?
 
-def Epsilon_C1pr(ħω):
-    return C1pr*E1pr**2/(E1pr**2-ħω**2-1j*ħω*Γ1pr)
+def Epsilon_Bx(ħω):
+#    n = 1
+#    Ex1 = E1 - G1/(n-0.5)**2
+#    ExΔ = E1+Δ1 - GΔ/(n-0.5)**2
+#    return  B1x/(Ex1-ħω-1j*Γ1) + B1sx/(ExΔ-ħω-1j*Γ1)
+# formula doesn't reproduce reported results. Using another formula from other
+# publications by same authors,
+# e.g. Ozaki and Adachi 1995, https://doi.org/10.1063/1.359966
+    return B1x/(E1-G1-ħω-1j*Γ1) + B1sx/(E1+Δ1-GΔ-ħω-1j*Γ1)
 
-def Epsilon_C21(ħω):
-    return C21*E21**2/(E21**2-ħω**2-1j*ħω*Γ21)
+def Epsilon_C(ħω):
+    χ2 = ħω/E2
+    return C/((1-χ2**2)-1j*χ2*γ)
 
-def Epsilon_C22(ħω):
-    return C22*E22**2/(E22**2-ħω**2-1j*ħω*Γ22)
-     
+ev_min=1.1
+ev_max=5.6
+npoints=500
 
-ev_min = 1.2
-ev_max = 5.6
-npoints = 500
 eV = np.linspace(ev_min, ev_max, npoints)
 μm = 4.13566733e-1*2.99792458/eV
-εA    = Epsilon_A(eV)
-εB    = Epsilon_B(eV)
-εBx   = Epsilon_Bx(eV)
-εC0pr = Epsilon_C0pr(eV)
-εC1pr = Epsilon_C1pr(eV)
-εC21  = Epsilon_C21(eV)
-εC22  = Epsilon_C22(eV)
-ε = εA + εB + εBx + εC0pr + εC1pr + εC21 + εC22
+εA = Epsilon_A(eV)
+εAx = Epsilon_Ax(eV)
+εB = Epsilon_B(eV)
+εBx = Epsilon_Bx(eV)
+εC = Epsilon_C(eV)
+ε = εA + εAx + εB + εBx + εC + εinf
 n = (ε**.5).real
 k = (ε**.5).imag
 α = 4*π*k/μm*1e4 #1/cm
@@ -109,24 +101,20 @@ plt.legend(bbox_to_anchor=(0,1.02,1,0),loc=3,ncol=2,borderaxespad=0)
 #plot intermediate data (for debugging)
 plt.figure(2)
 plt.plot(eV, εA.real, label="Re(εA)")
+plt.plot(eV, εAx.real, label="Re(εAx)")
 plt.plot(eV, εB.real, label="Re(εB)")
 plt.plot(eV, εBx.real, label="Re(εBx)")
-plt.plot(eV, εC0pr.real, label="Re(εC0pr)")
-plt.plot(eV, εC1pr.real, label="Re(εC1pr)")
-plt.plot(eV, εC21.real, label="Re(εC21)")
-plt.plot(eV, εC22.real, label="Re(εC22)")
+plt.plot(eV, εC.real, label="Re(εC)")
 plt.xlabel('Photon energy (eV)')
 plt.ylabel('ε')
 plt.legend(bbox_to_anchor=(0,1.02,1,0),loc=3,ncol=2,borderaxespad=0)
 
 plt.figure(3)
 plt.plot(eV, εA.imag, label="Im(εA)")
+plt.plot(eV, εAx.imag, label="Im(εAx)")
 plt.plot(eV, εB.imag, label="Im(εB)")
 plt.plot(eV, εBx.imag, label="Im(εBx)")
-plt.plot(eV, εC0pr.imag, label="Re(εC0pr)")
-plt.plot(eV, εC1pr.imag, label="Re(εC1pr)")
-plt.plot(eV, εC21.imag, label="Re(εC21)")
-plt.plot(eV, εC22.imag, label="Re(εC22)")
+plt.plot(eV, εC.imag, label="Im(εC)")
 plt.xlabel('Photon energy (eV)')
 plt.ylabel('ε')
 plt.legend(bbox_to_anchor=(0,1.02,1,0),loc=3,ncol=2,borderaxespad=0)
@@ -145,7 +133,6 @@ plt.plot(μm, n, label="n")
 plt.plot(μm, k, label="k")
 plt.xlabel('Wavelength (μm)')
 plt.ylabel('n, k')
-plt.xscale('log')
 plt.yscale('log')
 plt.legend(bbox_to_anchor=(0,1.02,1,0),loc=3,ncol=2,borderaxespad=0)
 
--- scripts/Ozaki 1993 - ZnS.py
+++ scripts/Ozaki 1993 - ZnS.py
@@ -1,41 +1,29 @@
 # -*- coding: utf-8 -*-
 # Author: Mikhail Polyanskiy
 # Last modified: 2017-04-12
-# Original data: Ozaki and Adachi 1995, https://doi.org/10.1063/1.359966
+# Original data: Ozaki and Adachi 1993, https://doi.org/10.1143/JJAP.32.5008
 
 import numpy as np
 import matplotlib.pyplot as plt
 π = np.pi
 
 # model parameters from table I
-E0   = 1.42    #eV
-Δ0   = 1.75-E0 #eV
-A    = 7.0     #eV**1.5
-Γ0   = 0.03    #eV
+E0   = 3.75    #eV
+Δ0   = 3.82-E0 #eV
+A    = 32.0    #eV**1.5
+A0x  = 0.07    #eV**-1
+G0   = 0.040   #eV
+Γ0   = 0.07    #eV
 
-E1   = 2.91    #eV
-Δ1   = 3.14-E1 #eV
-B1   = 3.50
-B2   = 1.75
-B1x  = 1.2     #eV
-B2x  = 0.6     #eV
-Γ1   = 0.12    #eV
+E1minusG1=5.74 #eV
+B1x  = 4.3     #eV
+Γ1   = 0.48    #eV
 
-E0pr = 4.45    #eV
-C0pr = 0.80
-Γ0pr = 0.67
+E2   = 7.0     #eV
+C    = 0.25
+γ    = 0.06
 
-E21  = 4.77    #eV
-C21  = 1.35
-Γ21  = 0.62
-
-E22  = 5.00    #eV
-C22  = 0.35
-Γ22  = 0.4     #eV
-
-E1pr = 6.6     #eV
-C1pr = 0.7
-Γ1pr = 0.6     #eV
+εinf = 2.35
 
 
 def Epsilon_A(ħω):
@@ -45,29 +33,18 @@ def Epsilon_A(ħω):
     fχs0 = χs0**-2 * ( 2 - (1+χs0)**0.5 - (1-χs0)**0.5 )
     return A*E0**-1.5 * (fχ0+0.5*(E0/(E0+Δ0))**1.5*fχs0)
 
-def Epsilon_B(ħω):
-    χ1d = (ħω + 1j*Γ1) / E1
-    χ1sd = (ħω + 1j*Γ1) / (E1+Δ1)
-    return -B1*χ1d**-2*np.log(1-χ1d**2) - B2*χ1sd**-2*np.log(1-χ1sd**2)
-
-def Epsilon_Bx(ħω):
+def Epsilon_Ax(ħω):
     y=0
     for n in range(1,1000):
-       y += 1/(2*n-1)**3 * ( B1x/(E1-ħω-1j*Γ1) + B2x/(E1+Δ1-ħω-1j*Γ1) )
-       #note: G1 = 0; G1s = 0
+        y += A0x/n**3 * ( 1/(E0-G0/n**2-ħω-1j*Γ0) + 0.5/(E0+Δ0-G0/n**2-ħω-1j*Γ0) )
     return y
 
-def Epsilon_C0pr(ħω):
-    return C0pr*E0pr**2/(E0pr**2-ħω**2-1j*ħω*Γ0pr)
+def Epsilon_Bx(ħω):
+    return B1x/(E1minusG1-ħω-1j*Γ1)
 
-def Epsilon_C1pr(ħω):
-    return C1pr*E1pr**2/(E1pr**2-ħω**2-1j*ħω*Γ1pr)
-
-def Epsilon_C21(ħω):
-    return C21*E21**2/(E21**2-ħω**2-1j*ħω*Γ21)
-
-def Epsilon_C22(ħω):
-    return C22*E22**2/(E22**2-ħω**2-1j*ħω*Γ22)
+def Epsilon_C(ħω):
+    χ2 = ħω/E2
+    return C/((1-χ2**2)-1j*χ2*γ)
      
 
 ev_min = 1.2
@@ -76,13 +53,10 @@ npoints = 500
 eV = np.linspace(ev_min, ev_max, npoints)
 μm = 4.13566733e-1*2.99792458/eV
 εA    = Epsilon_A(eV)
-εB    = Epsilon_B(eV)
+εAx   = Epsilon_Ax(eV)
 εBx   = Epsilon_Bx(eV)
-εC0pr = Epsilon_C0pr(eV)
-εC1pr = Epsilon_C1pr(eV)
-εC21  = Epsilon_C21(eV)
-εC22  = Epsilon_C22(eV)
-ε = εA + εB + εBx + εC0pr + εC1pr + εC21 + εC22
+εC    = Epsilon_C(eV)
+ε = εA + εAx + εBx + εC + εinf
 n = (ε**.5).real
 k = (ε**.5).imag
 α = 4*π*k/μm*1e4 #1/cm
@@ -109,24 +83,18 @@ plt.legend(bbox_to_anchor=(0,1.02,1,0),loc=3,ncol=2,borderaxespad=0)
 #plot intermediate data (for debugging)
 plt.figure(2)
 plt.plot(eV, εA.real, label="Re(εA)")
-plt.plot(eV, εB.real, label="Re(εB)")
+plt.plot(eV, εAx.real, label="Re(εAx)")
 plt.plot(eV, εBx.real, label="Re(εBx)")
-plt.plot(eV, εC0pr.real, label="Re(εC0pr)")
-plt.plot(eV, εC1pr.real, label="Re(εC1pr)")
-plt.plot(eV, εC21.real, label="Re(εC21)")
-plt.plot(eV, εC22.real, label="Re(εC22)")
+plt.plot(eV, εC.real, label="Re(εC)")
 plt.xlabel('Photon energy (eV)')
 plt.ylabel('ε')
 plt.legend(bbox_to_anchor=(0,1.02,1,0),loc=3,ncol=2,borderaxespad=0)
 
 plt.figure(3)
 plt.plot(eV, εA.imag, label="Im(εA)")
-plt.plot(eV, εB.imag, label="Im(εB)")
+plt.plot(eV, εAx.imag, label="Im(εAx)")
 plt.plot(eV, εBx.imag, label="Im(εBx)")
-plt.plot(eV, εC0pr.imag, label="Re(εC0pr)")
-plt.plot(eV, εC1pr.imag, label="Re(εC1pr)")
-plt.plot(eV, εC21.imag, label="Re(εC21)")
-plt.plot(eV, εC22.imag, label="Re(εC22)")
+plt.plot(eV, εC.imag, label="Re(εC)")
 plt.xlabel('Photon energy (eV)')
 plt.ylabel('ε')
 plt.legend(bbox_to_anchor=(0,1.02,1,0),loc=3,ncol=2,borderaxespad=0)
